a165ca8d57c73e42bb53c61f76350010b3cd28f5
carlos <2180687@my.ipleiria.pt>
2020-11-13 04:45:50 +0000

    Criação de modelos e controladores e views , programação na pagina pedidos(takeaway) parte 1, limpeza e melhoramento do codigo do layout main

--- GestorRestaurante/frontend/views/pedido/create.php
+++ GestorRestaurante/frontend/views/pedido/create.php
@@ -1,6 +1,8 @@
 <?php
 
+use kartik\datetime\DateTimePicker;
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Pedido */
@@ -13,8 +15,46 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <?php $id = Yii::$app->user->identity->id; ?>
+    <?php $form= ActiveForm::begin(); ?>
+
+
+    <?= $form->field($model, 'data')->widget(DateTimePicker::classname(), [
+    'options' => ['placeholder' => 'Enter event time ...'],
+    'type' =>DateTimePicker::TYPE_COMPONENT_APPEND,
+    'pluginOptions' => [
+    'todayBtn' => true,
+    'autoclose' => true,
+    'language'=>'pt-PT',
+
+    ]
+    ]);?>
+
+
+    <?= $form->field($model, 'estado')->textInput() ?>
+
+    <?php if(Yii::$app->authManager->getAssignment('cliente',$id)){
+     $form->field($model, 'tipo')->textInput(['tipo'=>'Takeaway','readonly'=>true]);
+    }else{
+
+    }
+    ?>
+
+    <?= $form->field($model, 'nome_pedido')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'id_mesa')->textInput() ?>
+
+    <?= $form->field($model, 'id_perfil')->textInput() ?>
+
+<div class="col-md-6">
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+
+</div>
 
 </div>

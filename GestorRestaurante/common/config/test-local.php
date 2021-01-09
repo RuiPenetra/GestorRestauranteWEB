6803830337f9cb1ef97c68e97f80657c44a9b367
Rui Penetra <2180680@my.ipleiria.pt>
2021-01-09 23:59:11 +0000

    Nova Alteração git ignore part2

---
+++ GestorRestaurante/common/config/test-local.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'dsn' => 'mysql:host=localhost;dbname=bdgestorrestaurante_tests',
+            'username' => 'root',
+            'password' => '',
+        ],
+    ],
+];
